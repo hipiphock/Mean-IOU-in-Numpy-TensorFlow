c6386d7c54a77b3164d89d12f0bd3e0cb3069afa
sp9103 <sp9103@naver.com>
2020-11-23 14:51:18 +0900

    making modules for each calculating system

--- meaniou_tensorflow.py
+++ meaniou_tensorflow.py
@@ -8,8 +8,10 @@ import tensorflow as tf
 # from tensorflow.compat.v1.keras import backend as K
 from tensorflow.keras import backend as K
 
-numpy_iou_result = []
-tf_iou_result = []
+iou_result = []
+iou_result.append(["index(filename)", "np_result", "tf_result"])
+np_iou_results = []
+tf_iou_results = []
 
 
 # write numpy_iou result to csv file
@@ -17,13 +19,13 @@ tf_iou_result = []
 # ...               ...             ...
 # total             np_total        tf_total
 def write_result_to_file(filename):
-    global numpy_iou_result
-    np.savetxt(filename, numpy_iou_result, delimiter=",")
+    global tf_iou_results
+    np.savetxt(filename, iou_result, delimiter=",")
 
 
 ## IOU in pure numpy
 def numpy_iou(y_true, y_pred, n_class=2):
-    global numpy_iou_result
+    global np_iou_results
     def iou(y_true, y_pred, n_class):
         # IOU = TP/(TP+FN+FP)
         IOU = []
@@ -48,7 +50,7 @@ def numpy_iou(y_true, y_pred, n_class=2):
     for idx in range(batch):
         iou_value = iou(y_true[idx], y_pred[idx], n_class)
         score.append(iou_value)
-        numpy_iou_result.append(iou_value)
+        np_iou_results.append(iou_value)
     return np.mean(score)
 
 
@@ -65,57 +67,65 @@ def numpy_mean_iou(y_true, y_pred):
 
 
 def tf_mean_iou(y_true, y_pred):
-    global tf_iou_result
+    global tf_iou_results
     prec = []
     for t in np.arange(0.5, 1.0, 0.5):
         y_pred_ = tf.cast(y_pred > t, tf.int32)
         score, up_opt = tf.metrics.mean_iou(y_true, y_pred_, 2)
         K.get_session().run(tf.local_variables_initializer())
         prec.append(score)
-        tf_iou_result.append(score)
     val = K.mean(K.stack(prec), axis=0)
     return [val, up_opt]
+
+def tf_mean_iou_for_single_instance(y_true, y_pred):
+    global tf_iou_results
+    for t in np.arange(0.5, 1.0, 0.5):
+        y_pred_ = tf.cast(y_pred > t, tf.int32)
+        score, up_opt = tf.metrics.mean_iou(y_true, y_pred_, 2)
+        K.get_session().run(tf.local_variables_initializer())
+        prec.append(score)
+    val = K.mean(K.stack(prec), axis=0)
+    return val
 
 if __name__ == "__main__":
     ## Seeding
     tf.compat.v1.random.set_random_seed(1234)
 
     ## Defining Placeholders
-    shape = [106, 720, 1280, 1]
+    shape = [1, 720, 1280, 1]
     y_true = tf.placeholder(tf.int32, shape=shape)
     y_pred = tf.placeholder(tf.int32, shape=shape)
 
     ## Reading the masks from the path
-    y_true_masks = np.zeros((106, 720, 1280, 1), dtype=np.int32)    # 직접 레이블링 한거
-    y_pred_masks = np.zeros((106, 720, 1280, 1), dtype=np.int32)    # detect 된거
     for idx, path in enumerate(os.listdir("target/")):              # 어차피 target이랑 test랑 파일명 같아
+        y_true_masks = np.zeros((1, 720, 1280, 1), dtype=np.int32)    # 직접 레이블링 한거
+        y_pred_masks = np.zeros((1, 720, 1280, 1), dtype=np.int32)    # detect 된거
         mask = cv2.imread("target/" + path, -1)
         mask = np.expand_dims(mask, axis=-1)
+        # TODO: current masking process takes a lot of time.
+        # This process need to be deleted, by changing mean_iou function.
         for i in range(720):
             for j in range(1280):
                 if mask[i][j] != 0:
                     mask[i][j] = 1
-        y_true_masks[idx] = mask
+        y_true_masks[0] = mask
         mask = cv2.imread("test/" + path, -1)
         mask = np.expand_dims(mask, axis=-1)
         for i in range(720):
             for j in range(1280):
                 if mask[i][j] != 0:
                     mask[i][j] = 1
-        y_pred_masks[idx] = mask
+        y_pred_masks[0] = mask
 
-    ## Session
-    with tf.compat.v1.Session() as sess:
-        sess.run(tf.local_variables_initializer())
-        sess.run(tf.global_variables_initializer())
+        ## Session
+        with tf.compat.v1.Session() as sess:
+            sess.run(tf.local_variables_initializer())
+            sess.run(tf.global_variables_initializer())
 
-        ## Mean IOU
-        miou = numpy_mean_iou(y_true, y_pred)
-        miou = sess.run(miou, feed_dict={y_true: y_true_masks, y_pred: y_pred_masks})
-        print("Numpy mIOU: ", miou)
-        write_result_to_file("results/np_results.csv")
+            tf_miou, conf = tf_mean_iou(y_true, y_pred)
+            sess.run(conf, feed_dict={y_true: y_true_masks, y_pred: y_pred_masks})
+            tf_miou = sess.run(tf_miou, feed_dict={y_true: y_true_masks, y_pred: y_pred_masks})
+            print("TF mIOU: ", tf_miou)
+            tf_iou_results.append(tf_miou)
 
-        miou, conf = tf_mean_iou(y_true, y_pred)
-        sess.run(conf, feed_dict={y_true: y_true_masks, y_pred: y_pred_masks})
-        miou = sess.run(miou, feed_dict={y_true: y_true_masks, y_pred: y_pred_masks})
-        print("TF mIOU: ", miou)
+    write_result_to_file("results/tf_results.csv")
